e1afe3c41154975aa702bc222e630bebacedeb44
tallesfelix <tallesfelixg95@gmail.com>
2018-05-01 01:52:32 -0300

    Adicionado Navbar integrada com devise

---
+++ db/migrate/20180501043443_add_parameters_to_user.rb
@@ -0,0 +1,13 @@
+class AddParametersToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :cpf, :string
+    add_column :users, :birth, :date
+    add_column :users, :maritalstatus, :string
+    add_column :users, :telephone, :string
+    add_column :users, :neighborhood, :string
+    add_column :users, :state, :string
+    add_column :users, :city, :string
+    add_column :users, :street, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_01_042331) do
+ActiveRecord::Schema.define(version: 2018_05_01_043443) do
 
   create_table "causes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "problem"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2018_05_01_042331) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "cpf"
+    t.date "birth"
+    t.string "maritalstatus"
+    t.string "telephone"
+    t.string "neighborhood"
+    t.string "state"
+    t.string "city"
+    t.string "street"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
