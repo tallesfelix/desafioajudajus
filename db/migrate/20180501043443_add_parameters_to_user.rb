429e19fbf508b94c303a1d95027b7d6b1c3a1511
tallesfelix <tallesfelixg95@gmail.com>
2018-05-01 02:32:33 -0300

    Editando as views do devise

--- db/migrate/20180501043443_add_parameters_to_user.rb
+++ db/migrate/20180501043443_add_parameters_to_user.rb
@@ -2,7 +2,7 @@ class AddParametersToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string
     add_column :users, :cpf, :string
-    add_column :users, :birth, :date
+    add_column :users, :birth, :datetime
     add_column :users, :maritalstatus, :string
     add_column :users, :telephone, :string
     add_column :users, :neighborhood, :string
